f2ba984b3b0bd63b8d8c1697e78d5c47abafa295
Maria Rjabtseva <nkdsnake@tlu.ee>
2026-05-02 23:28:08 +0300

    tablet and desktop view style added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const overlay = document.querySelector('.overlay');
+const overlaybtns = document.querySelector('.overlay-buttons');
 
-function handleClick(e) {
+/*function handleClick(e) {
     if (e.target === overlay) {
         closeOverlay();
     }
@@ -17,4 +18,21 @@ function closeOverlay() {
     overlay.style.display = 'none';
 
     overlay.removeEventListener('click', handleClick);
+}*/
+
+function toggleOverlay() {
+    overlay.classList.add('active');
+    overlaybtns.classList.add('active');
+    overlay.addEventListener('click', handleClick);
+}
+
+function closeOverlay() {
+    overlay.classList.remove('active');
+    overlaybtns.classList.remove('active');
+}
+
+function handleClick(e) {
+    if (e.target === overlay) {
+        closeOverlay();
+    }
 }
